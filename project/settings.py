963e559ad3c49f7d72053a64ef19ed67a449b0e5
HalmuratSarazhiev <khalmurat.sarazhiev@gmail.com>
2022-06-14 18:24:22 +0600

    my_commit_update

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # libs
+    'rest_framework',
+
+
+
+
+    #applications:
+    'applications.category',
+    'applications.order',
+    'applications.user',
+
 ]
 
 MIDDLEWARE = [
@@ -105,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
